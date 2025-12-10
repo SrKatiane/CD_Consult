cb9854a9e91e1a1d8d42763520faf5360473d87a
skatiane <katianeribeiro.s@outlook.com.br>
2025-12-10 14:46:16 -0300

    fix: scrollToTop

--- src/pages/PrivacyPolicy.tsx
+++ src/pages/PrivacyPolicy.tsx
@@ -1,8 +1,14 @@
 import { Helmet } from "react-helmet";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { useEffect } from "react";
 
 const PrivacyPolicy = () => {
+
+    useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  
   return (
     <>
       <Helmet>
